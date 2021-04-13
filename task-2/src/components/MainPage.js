95e852ef7b69cc49720e50aed1ac5d49704c9137
unknown <asanashik7@gmail.com>
2021-04-13 12:15:35 +0530

    Task2 completed files and output snap added

--- task-2/src/components/MainPage.js
+++ task-2/src/components/MainPage.js
@@ -19,6 +19,9 @@ function MainPage(){
             setInputError("Invalid input, Kindly check it");
         }
         else{
+            setInputError('')
+            setDataError("")
+            setInput('')
             setData([...data, 
                 {id : data.length , value : input}])
         }
@@ -26,31 +29,34 @@ function MainPage(){
 
     }
 
-    function remove(index){
+    function remove(index,value){
         console.log(index)
         let message =  window.confirm("Remove Data?");
         console.log(message)
+        const list = [...data]
         if(message === true){
             
             //delete data[index.value];
-            // delete data[index];  
+            //delete data[index];  
             //data.filter((item) => item.index !== index);
             //const newList = data.splice(index,index-1);
-            return data.splice(index,1)
-
+            //list.splice(data.indexOf(value),1);
+            list.splice(index,1);
+            setData(list)
             //setData()
             //return data;
             //setUpdatedData([...updatedData, newData]);
             //setData(data.splice(index,1));
         }
-        if(data === []){
-            setDataError("NO DATA")
-        }
+
+        (list.length === 0)?setDataError("NO DATA"):setDataError("")
+        
     }
 
 
     return(
         <div className="Border">
+            <h2>ADD AND REMOVE</h2>
             <h5 className="color">{inputError}</h5>
 
             <div className="input-group mb-3">
@@ -63,7 +69,7 @@ function MainPage(){
             <h5 className="color">{dataError}</h5>
             <ul>
                 {data.map(item => (
-                    <li key={item.id}>{item.value} &nbsp;&nbsp;<button className="btn btn-primary" onClick={()=>remove(item.id)}>REMOVE</button><br/></li>
+                    <li key={item.id}>{item.value} &nbsp;&nbsp;<button className="btn btn-primary" onClick={()=>remove(item.id,item.value)}>REMOVE</button><br/></li>
                 ))}
             </ul>
         </div>
